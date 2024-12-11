dcd51aee2017be20d81154c92516a9296a2b18cf
347Online | Katie Janzen <katiejanzen@347online.me>
2024-12-11 02:32:51 -0600

    Accept first param as a dir or default to current dir

--- gen.pl
+++ gen.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/env perl
+my $dir = @ARGV[0] // ".";
 
-opendir (DIR, './posts') or die "Couldn't open directory, $!";
+opendir (DIR, $dir) or die "Couldn't open directory, $!";
 foreach (grep(/^[^\.].*\.md$/, readdir(DIR))) {
   print "$_\n";
 }
+
 closedir DIR;
