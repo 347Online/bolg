2b6a3617e5f24d6f886227df3993b35e8ea408d6
347Online | Katie Janzen <katiejanzen@347online.me>
2024-12-11 03:34:03 -0600

    Ignore posts

--- gen.pl
+++ gen.pl
@@ -4,10 +4,9 @@ my $dir = @ARGV[0] // ".";
 opendir (DIR, $dir) or die "Couldn't open directory, $!";
 foreach (grep(/^[^\.].*\.md$/, readdir(DIR))) {
   my $fname = `readlink -f $dir/$_`; 
-  my $in = $fname;
-  chomp($in);
-  my $out = $in =~ s/.md/.html/r;
-  my $f = $in;
+  chomp($fname);
+  my $out = $fname =~ s/.md/.html/r;
+  my $f = $fname;
   open(FH, ">", $out) or die $!;
   my $html = `markdown $f`;
   print FH $html;
